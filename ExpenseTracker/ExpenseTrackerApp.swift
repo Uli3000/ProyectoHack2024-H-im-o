5bb3ee1a2ed3ce710c7944143a196111b2276fd3
IvanRincon18 <ivanrincon471@gmail.com>
2024-09-15 00:13:29 -0600

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ivan Jhair Gomez Rincon on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

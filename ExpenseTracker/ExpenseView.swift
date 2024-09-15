a938a6022d37b179e6b26baab52c3c97d5b11d23
Uli3000 <ulisesgamer281@gmail.com>
2024-09-15 10:15:10 -0600

    Final Proyect

---
+++ ExpenseTracker/ExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseView.swift
+//  ExpenseTracker
+//
+//  Created by Ivan Jhair Gomez Rincon on 15/09/24.
+//
+
+import SwiftUI
+
+struct ExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseView()
+}
---
+++ ExpenseTracker/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  ExpenseTracker
+//
+//  Created by Ivan Jhair Gomez Rincon on 15/09/24.
+//
+
+import Foundation
